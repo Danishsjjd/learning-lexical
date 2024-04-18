35cc251630a70512dfaf1f1b86189625aa18fc94
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-04-18 13:33:37 +0500

    feat: multiple editor setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Editor from "./Editor"
+import { EditorProvider } from "./Editor/context/EditorProvider"
 
 const App = () => {
-  return <Editor />
+  return (
+    <EditorProvider>
+      <Editor id="1" />
+      <Editor id="2" />
+    </EditorProvider>
+  )
 }
 
 export default App
--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -4,6 +4,7 @@ import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary"
 import ReactPopulateRichText from "./plugins/ReactPopulateRichText"
 import { RichTextPlugin } from "./plugins/ReactRichTextPlugin"
 import editorTheme from "./theme"
+import { MultipleEditorStorePlugin } from "./plugins/MultipleEditorStorePlugin"
 
 const config: InitialConfigType = {
   namespace: "Lexical markdown editor",
@@ -14,19 +15,21 @@ const config: InitialConfigType = {
   theme: editorTheme,
 }
 
-const Editor = () => {
+const Editor = ({ id }: { id: string }) => {
   return (
     <LexicalComposer initialConfig={config}>
-      <ReactPopulateRichText />
       <section className="max-w-2xl mx-auto my-6 rounded-xl border relative overflow-hidden">
         <RichTextPlugin
-          contentEditable={<ContentEditable className="py-4 px-3 rounded-xl prose" />}
+          contentEditable={<ContentEditable className="py-4 px-3 w-full max-w-2xl rounded-xl prose" />}
           placeholder={
             <div className="text-gray-400 overflow-hidden absolute top-4 left-3">Enter some rich text...</div>
           }
           ErrorBoundary={LexicalErrorBoundary}
         />
       </section>
+
+      <ReactPopulateRichText />
+      <MultipleEditorStorePlugin id={id} />
     </LexicalComposer>
   )
 }
