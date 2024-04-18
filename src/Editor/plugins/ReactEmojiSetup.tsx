1300d3d842a6bfff2c1d1a0d0e4d4a728ef6a0f8
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-04-18 13:47:48 +0500

    fix: jsx lint warn

--- src/Editor/plugins/ReactEmojiSetup.tsx
+++ src/Editor/plugins/ReactEmojiSetup.tsx
@@ -2,10 +2,12 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import useLayoutEffectImpl from "../shared/useLayoutEffect"
 import { registerEmoji } from "./emoji/EmojiPlugin"
 
-export function ReactEmojiSetup(): void {
+export function ReactEmojiSetup() {
   const [editor] = useLexicalComposerContext()
 
   useLayoutEffectImpl(() => {
     return registerEmoji(editor)
   }, [editor])
+
+  return null
 }
