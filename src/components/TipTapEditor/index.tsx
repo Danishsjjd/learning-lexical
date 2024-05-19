49110d636823c54a60c009a0343704ac4c9244f7
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-05-19 08:38:21 +0500

    feat: tiptap debug ui

--- src/components/TipTapEditor/index.tsx
+++ src/components/TipTapEditor/index.tsx
@@ -1,16 +1,24 @@
 import { Color } from "@tiptap/extension-color"
-import { Underline } from "@tiptap/extension-underline"
-import { EditorProvider } from "@tiptap/react"
-import { StarterKit } from "@tiptap/starter-kit"
-import { containerClasses, sectionClasses, sections } from "../../data"
-import TextStyle from "@tiptap/extension-text-style"
-import ToolBar from "./Toolbar"
-import { Markdown } from "tiptap-markdown"
 import { Image as ImagePlugin } from "@tiptap/extension-image"
+import TextStyle from "@tiptap/extension-text-style"
+import { Underline } from "@tiptap/extension-underline"
+import { Editor, EditorProvider } from "@tiptap/react"
+import { StarterKit } from "@tiptap/starter-kit"
+import { Markdown } from "tiptap-markdown"
+import { containerClasses, sectionClasses, sections } from "../../data"
+import ToolBar from "./Toolbar"
+import { useState } from "react"
 
 const TiptapEditor = () => {
+  const [jsonString, setJsonString] = useState("")
+
+  const updateJSON = (editor: Editor) => {
+    const stringJSON = JSON.stringify(editor.getJSON(), null, 2)
+    setJsonString(stringJSON)
+  }
+
   return (
-    <section>
+    <section className="mx-auto max-w-2xl overflow-hidden">
       <EditorProvider
         slotBefore={<ToolBar />}
         extensions={[
@@ -26,13 +34,18 @@ const TiptapEditor = () => {
           }),
         ]}
         content={sections[0].section}
+        onCreate={({ editor }) => updateJSON(editor as Editor)}
         onUpdate={({ editor }) => {
+          updateJSON(editor as Editor)
           console.log(editor.storage.markdown.getMarkdown())
         }}
         editorProps={{
           attributes: { class: `${containerClasses} ${sectionClasses}` },
         }}
       />
+      <div className="tree-view-output">
+        <pre>{jsonString}</pre>
+      </div>
     </section>
   )
 }
